91adc49586320e16b12005871c9b89c6527b8381
Cemil Ülay <cemilulay@gmail.com>
2023-03-01 12:43:44 +0100

    create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className='bg-slate-100 border-t-2'>
+      <div className='max-w-4xl mx-auto py-5'>
+        <h4 className='flex text-gray-400 justify-center md:text-lg mx-6'>
+          Movieme 2023 by CU
+        </h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
