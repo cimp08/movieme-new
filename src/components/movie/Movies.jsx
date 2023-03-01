6a001514d8bd0a6b6e3c305364cb364850382e71
Cemil Ülay <cemilulay@gmail.com>
2023-03-01 17:46:34 +0100

    import Slider, DiscoverList and GenreList

--- src/components/movie/Movies.jsx
+++ src/components/movie/Movies.jsx
@@ -1,7 +1,21 @@
+import Slider from './Slider';
+import DiscoverList from './DiscoverList';
+import GenresList from './GenresList';
+
 const Movies = () => {
   return (
-    <div>START PAGE</div>
-  )
-}
+    <>
+      <Slider />
+      <div className='flex flex-col md:flex-row gap-6 container mx-auto mt-10'>
+        <div className='md:flex-initial md:w-[400px]'>
+          <DiscoverList />
+        </div>
+        <div className='flex-auto'>
+          <GenresList />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Movies
+export default Movies;
