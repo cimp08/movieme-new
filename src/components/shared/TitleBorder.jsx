dbd590d6ecb2730ec3e575d9fd3dee0dd18d85e4
Cemil Ülay <cemilulay@gmail.com>
2023-03-01 12:44:38 +0100

    add folder shared, create shared TitleBorder component

---
+++ src/components/shared/TitleBorder.jsx
@@ -0,0 +1,13 @@
+const TitleBorder = () => {
+  return (
+    <span className='block mb-10'>
+      <h4 className='relative text-lg text-center border-b'>
+        <span className='bg-white text-purple-500 relative top-[13px] px-[10px]'>
+          Discover
+        </span>
+      </h4>
+    </span>
+  );
+}
+
+export default TitleBorder
