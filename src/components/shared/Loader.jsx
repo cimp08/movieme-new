962605a31209faa36a656a17590702cab3c0a7ba
Cemil Ülay <cemilulay@gmail.com>
2023-03-03 10:44:54 +0100

    create a loader, and a component for errors

---
+++ src/components/shared/Loader.jsx
@@ -0,0 +1,24 @@
+const Loader = () => {
+  return (
+    <div className="flex gap-5">
+      <div
+        className='w-12 h-12 rounded-full animate-spin
+                    border-y-8 border-solid border-purple-500 border-t-transparent shadow-md'
+      ></div>
+      <div
+        className='w-12 h-12 rounded-full animate-spin
+                    border-y-8 border-solid border-purple-500 border-t-transparent shadow-md'
+      ></div>
+      <div
+        className='w-12 h-12 rounded-full animate-spin
+                    border-y-8 border-solid border-purple-500 border-t-transparent shadow-md'
+      ></div>
+      <div
+        className='w-12 h-12 rounded-full animate-spin
+                    border-y-8 border-solid border-purple-500 border-t-transparent shadow-md'
+      ></div>
+    </div>
+  );
+};
+
+export default Loader;
---
+++ src/components/shared/QuestionMark.jsx
@@ -0,0 +1,10 @@
+
+const QuestionMark = () => {
+  return (
+    <div className='animate-bounce'>
+      <img src='/assets/images/question.png' width={100} alt="question" />
+    </div>
+  );
+};
+
+export default QuestionMark;
