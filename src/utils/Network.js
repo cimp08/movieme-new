eae24f745e9f678b103746628ee6f2cf21456ca9
Cemil Ülay <cemilulay@gmail.com>
2023-03-07 18:56:13 +0100

    create mediaCastsUrl function

--- src/utils/Network.js
+++ src/utils/Network.js
@@ -9,10 +9,14 @@ export const sliderHelper = {
 
 // fetching a list of movie genres.
 // fetching a movie or tv based on type details
+// fetching casts from movie or tv based on type and id
 export const mediaHelper = {
   genreUrl: `${baseUrl}/genre/movie/list?api_key=${API}&language=en-US`,
   mediaUrl: (type, id) => {
     return `${baseUrl}/${type}/${id}?api_key=${API}&language=en-US`;
+  },
+  mediaCastsUrl: (type, id) => {
+    return `${baseUrl}/${type}/${id}/credits?api_key=${API}`;
   }
 };
 
