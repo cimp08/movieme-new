9f87ba5866f7bd2fc2ab00d995e8cf0b4f046d6f
Cemil Ülay <cemilulay@gmail.com>
2023-03-07 17:46:10 +0100

    title as props

--- src/components/shared/TitleBorder.jsx
+++ src/components/shared/TitleBorder.jsx
@@ -1,9 +1,9 @@
-const TitleBorder = () => {
+const TitleBorder = ({title}) => {
   return (
     <span className='block mb-10'>
       <h4 className='relative text-lg text-center border-b'>
         <span className='bg-white text-purple-500 relative top-[13px] px-[10px]'>
-          Discover
+          {title}
         </span>
       </h4>
     </span>
