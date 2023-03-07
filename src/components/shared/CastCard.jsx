5475262edb68695b103755af3674c2773ae6e235
Cemil Ülay <cemilulay@gmail.com>
2023-03-07 23:05:02 +0100

    if theres no image for cast show user photo and switch to decide what kind of gender

--- src/components/shared/CastCard.jsx
+++ src/components/shared/CastCard.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import { BASE_CAST_URL } from '../../utils/Constans';
+import React, { useState, useEffect } from 'react';
+import {
+  BASE_CAST_URL,
+  DEFAULT_USER_FEMALE,
+  DEFAULT_USER_MALE,
+  DEFAULT_USER_NO_GENDER,
+} from '../../utils/Constans';
 
 const CastCard = ({ cast }) => {
+  const [gender, setGender] = useState(null);
+
+  useEffect(() => {
+    switch (cast?.gender) {
+      case 0:
+        setGender(DEFAULT_USER_NO_GENDER);
+        break;
+      case 1:
+        setGender(DEFAULT_USER_FEMALE);
+        break;
+      case 2:
+        setGender(DEFAULT_USER_MALE);
+        break;
+      default:
+        setGender(DEFAULT_USER_NO_GENDER);
+        break;
+    }
+  }, [cast?.gender]);
+
   console.log(cast);
   return (
     <div className='flex flex-col items-center'>
       <img
-        src={BASE_CAST_URL + cast?.profile_path}
+        src={cast?.profile_path ? BASE_CAST_URL + cast?.profile_path : gender}
         alt={cast?.name}
-        className='w-[100px] h-[100px] object-cover rounded-full'
+        className='bg-gray-200 w-[100px] h-[100px] object-cover rounded-full'
       />
       <div className='text-center w-[100px] md:w-full'>
         <h3 className='mt-1 text-base font-bold text-gray-700'>{cast?.name}</h3>
