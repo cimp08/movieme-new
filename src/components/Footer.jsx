3252002e32e975402c231cf3a0162e81d2a44c02
Cemil Ülay <cemilulay@gmail.com>
2023-03-08 10:02:11 +0100

    add switcher to navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='bg-slate-100 border-t-2'>
+    <footer className='bg-slate-100 dark:bg-gray-900 border-t-2 dark:border-gray-900 flex-none'>
       <div className='max-w-4xl mx-auto py-5'>
         <h4 className='flex text-gray-400 justify-center md:text-lg mx-6'>
           Movieme 2023 by CU
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,55 @@
-import React from 'react'
+import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Switcher from './shared/Switcher';
+import {FaSearch} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
-    <nav className='bg-slate-100 shadow'>
-      <div className='max-w-4xl mx-auto py-5'>
-        <ul className='flex justify-between font-medium md:text-lg mx-6'>
-          <li>
-            <NavLink
-              to='/movies'
-              style={({ isActive }) => ({
-                color: isActive ? '#a855f7' : '',
-              })}
-            >
-              Movies
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/tv'
-              style={({ isActive }) => ({
-                color: isActive ? '#a855f7' : '',
-              })}
-            >
-              Tv-Shows
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to='/actors'
-              style={({ isActive }) => ({
-                color: isActive ? '#a855f7' : '',
-              })}
-            >
-              Actors
-            </NavLink>
-          </li>
-        </ul>
+    <nav className='bg-slate-100 dark:bg-gray-900 shadow flex-none'>
+      <div className='flex justify-between md:justify-around items-center py-5'>
+        <div className='ml-2 pb-[2px]'>
+          <Switcher />
+        </div>
+        <div>
+          <ul className='flex gap-12 md:gap-40 font-medium md:text-lg mx-6'>
+            <li>
+              <NavLink
+                to='/movies'
+                style={({ isActive }) => ({
+                  color: isActive ? '#a855f7' : '',
+                })}
+              >
+                Movies
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='/tv'
+                style={({ isActive }) => ({
+                  color: isActive ? '#a855f7' : '',
+                })}
+              >
+                Tv-Shows
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='/actors'
+                style={({ isActive }) => ({
+                  color: isActive ? '#a855f7' : '',
+                })}
+              >
+                Actors
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className='mr-2 pb-[2px]'>
+          <FaSearch />
+        </div>
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
