61b38245451658ac223eec03b58a86e5a9162b8c
Cemil Ülay <cemilulay@gmail.com>
2023-03-13 11:31:12 +0100

    add hover effect

--- src/components/shared/CastCard.jsx
+++ src/components/shared/CastCard.jsx
@@ -28,14 +28,16 @@ const CastCard = ({ cast }) => {
 
   console.log(cast);
   return (
-    <div className='flex flex-col items-center'>
+    <div className='group flex flex-col items-center cursor-pointer'>
       <img
         src={cast?.profile_path ? BASE_CAST_URL + cast?.profile_path : gender}
         alt={cast?.name}
-        className='bg-gray-200 w-[100px] h-[100px] object-cover rounded-full'
+        className='group-hover:brightness-200 duration-300 bg-gray-200 w-[100px] h-[100px] object-cover rounded-full'
       />
       <div className='text-center w-[100px] md:w-full'>
-        <h3 className='mt-1 text-base font-bold text-gray-700 dark:text-white'>{cast?.name}</h3>
+        <h3 className='mt-1 text-base font-bold text-gray-700 dark:text-white'>
+          {cast?.name}
+        </h3>
         <h4 className='text-gray-500 text-base'>{cast?.character}</h4>
       </div>
     </div>
