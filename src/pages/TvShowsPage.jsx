8ae12739c59d8be0f09fa7409e5bedddefd5e41c
Cemil Ülay <cemilulay@gmail.com>
2023-03-19 14:33:31 +0100

    add TvShowsPage

--- src/pages/TvShowsPage.jsx
+++ src/pages/TvShowsPage.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import Movies from '../components/movies/Movies';
 import Slider from '../components/shared/Slider';
+import DiscoverListTv from '../components/tv/DiscoverListTv';
+import TvShows from '../components/tv/tvShows';
 
 const TvShowsPage = () => {
   return (
     <>
       <Slider type='tv' />
+      <div className='container mx-auto mt-10'>
+          <DiscoverListTv />
+        <div className='flex-auto mx-4'>
+          <TvShows />
+        </div>
+      </div>
     </>
   );
 };
