34cb047d3d4eea73743e52028286c130a2261218
Cemil Ülay <cemilulay@gmail.com>
2023-03-19 13:57:09 +0100

    tailwind/css changes

--- src/components/tv/SeasonDetails.jsx
+++ src/components/tv/SeasonDetails.jsx
@@ -7,7 +7,7 @@ const SeasonDetails = ({ item }) => {
   return (
     <div>
       <TitleBorder title='Season Overview' />
-      <div className='flex gap-6 items-center'>
+      <div className='flex gap-3 md:gap-6 items-center'>
         <img
           src={
             item?.seasons[item?.seasons.length - 1]?.poster_path
@@ -19,18 +19,22 @@ const SeasonDetails = ({ item }) => {
           className='w-[200px]'
         />
         <div>
-          <h3 className='text-xl mb-1'>Current Season</h3>
-          <h4>{item?.seasons[item?.seasons.length - 1]?.name}</h4>
+          <h3 className='text-base md:text-xl mb-1'>Current Season</h3>
+          <h4 className='text-sm md:text-lg'>
+            {item?.seasons[item?.seasons.length - 1]?.name}
+          </h4>
           <span className='text-sm text-gray-400'>
             {item?.seasons[item?.seasons.length - 1]?.air_date.slice(0, 4)} |{' '}
             {item?.seasons[item?.seasons.length - 1]?.episode_count} Episodes
           </span>
-          <p className='mt-5'>
+          <p className='text-sm md:text-base mt-5'>
             {item?.seasons[item?.seasons.length - 1]?.overview
               ? item?.seasons[item?.seasons.length - 1]?.overview
               : `${item?.seasons[item?.seasons.length - 1]?.name} of the ${
                   item?.name
-                } premired on ${item?.seasons[item?.seasons.length - 1]?.air_date}`}
+                } premired on ${
+                  item?.seasons[item?.seasons.length - 1]?.air_date
+                }.`}
           </p>
         </div>
       </div>
