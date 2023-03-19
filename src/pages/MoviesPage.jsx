c64b52f18716009a44ef60197581bf9db10e7118
Cemil Ülay <cemilulay@gmail.com>
2023-03-19 14:32:11 +0100

    import DiscoverListMovie

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,5 +1,5 @@
 import Slider from '../components/shared/Slider';
-import DiscoverList from '../components/movies/DiscoverList';
+import DiscoverListMovie from '../components/movies/DiscoverListMovie';
 import GenresList from '../components/movies/GenresList';
 import Movies from '../components/movies/Movies';
 
@@ -10,7 +10,7 @@ const MoviesPage = () => {
       <Slider type='movie' />
       <div className='flex flex-col md:flex-row gap-6 container mx-auto mt-10'>
         <div className='md:flex-initial md:w-[400px] mx-4'>
-          <DiscoverList />
+          <DiscoverListMovie />
         </div>
         <div className='flex-auto mx-4'>
           <GenresList />
