dbb882a37964408cca2e6c26527669dbf4c06f1b
Cemil Ülay <cemilulay@gmail.com>
2023-03-19 01:59:21 +0100

    add tvShows page with slider so far...

---
+++ src/pages/TvShowsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Slider from '../components/shared/Slider';
+
+const TvShowsPage = () => {
+  return (
+    <>
+      <Slider type='tv' />
+    </>
+  );
+};
+
+export default TvShowsPage;
