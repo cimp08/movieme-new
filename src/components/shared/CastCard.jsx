b3963addd593b53384dec664f2b17df9a2979553
Cemil Ülay <cemilulay@gmail.com>
2023-03-19 13:56:07 +0100

    tailwind/css changes

--- src/components/shared/CastCard.jsx
+++ src/components/shared/CastCard.jsx
@@ -34,10 +34,10 @@ const CastCard = ({ cast }) => {
         className='group-hover:brightness-200 duration-300 bg-gray-200 w-[100px] h-[100px] object-cover rounded-full'
       />
       <div className='text-center w-[100px] md:w-full'>
-        <h3 className='mt-1 text-base font-bold text-gray-700 dark:text-white'>
+        <h3 className='mt-1 text-sm md:text-base font-bold text-gray-700 dark:text-white'>
           {cast?.name}
         </h3>
-        <h4 className='text-gray-500 text-base'>{cast?.character}</h4>
+        <h4 className='text-gray-500 text-sm md:text-base'>{cast?.character}</h4>
       </div>
     </div>
   );
