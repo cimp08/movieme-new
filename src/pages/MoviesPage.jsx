07c24cb557a942cf819e32dcba15ae168265b772
Cemil Ülay <cemilulay@gmail.com>
2023-03-19 01:55:22 +0100

    add prop type to slider

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,19 +1,20 @@
-import Slider from '../components/movies/Slider';
+import Slider from '../components/shared/Slider';
 import DiscoverList from '../components/movies/DiscoverList';
 import GenresList from '../components/movies/GenresList';
 import Movies from '../components/movies/Movies';
 
+
 const MoviesPage = () => {
   return (
     <>
-      <Slider />
+      <Slider type="movie" />
       <div className='flex flex-col md:flex-row gap-6 container mx-auto mt-10'>
         <div className='md:flex-initial md:w-[400px]'>
           <DiscoverList />
         </div>
         <div className='flex-auto'>
           <GenresList />
-          <Movies  />
+          <Movies />
         </div>
       </div>
     </>
