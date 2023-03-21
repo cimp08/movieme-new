ecbfa3b1f94975430672dcbc497bb4db116a44d2
Cemil Ülay <cemilulay@gmail.com>
2023-03-21 20:29:21 +0100

    add actors page

---
+++ src/pages/ActorsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Actors from '../components/actors/Actors';
+
+const ActorsPage = () => {
+  return (
+    <div className='max-w-6xl mx-auto'>
+      <div className='mx-4'>
+        <Actors />
+      </div>
+    </div>
+  );
+};
+
+export default ActorsPage;
