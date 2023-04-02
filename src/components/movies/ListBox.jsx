d29f5acba2c3f07d101e788d0647b4f2fb7c0835
Cemil Ülay <cemilulay@gmail.com>
2023-04-02 13:17:36 +0200

    tailwind changes

--- src/components/movies/ListBox.jsx
+++ src/components/movies/ListBox.jsx
@@ -34,7 +34,7 @@ const ListBox = ({ genres, setGenre, selectedGenres, setSelectedGenres }) => {
                   key={personIdx}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? 'bg-purple-400 text-white' : 'text-gray-900'
+                      active ? 'md:bg-purple-400 md:text-white' : 'text-gray-900'
                     }`
                   }
                   value={genre.name}
